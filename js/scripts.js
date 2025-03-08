4148421cbdd7ea39cd1cd4c2d93b82b5e221cbf0
Michele <cancilla.michele@gmail.com>
2025-03-08 16:34:30 +0100

    Aggiornato deploy rsvp mail+excel

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
         // && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
         // $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         // } else         {
-        $.post('https://script.google.com/macros/s/AKfycbwLRzcg9iSKZqGDWAFyG0jzHFzesIsRuw1kpJky9uBdIR4ri__A4pO-9jX-SxGRxjuH_w/exec', data)
+        $.post('https://script.google.com/macros/s/AKfycbwibH25JBX2m7RKlw1DYGnlcry3o17gtIOcJF2cUBr3MuieYliwXrkysRxk97N7CCMwOQ/exec', data)
             .done(function (data) {
                 console.log(data);
                 if (data.result === "error") {
